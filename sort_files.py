e90c8574f93e4fb4ec4ed62189430f78110ba209
hashi <kumarnisit@gmail.com>
2019-10-20 19:10:14 +0530

    modified to browse into sub-directory

--- sort_files.py
+++ sort_files.py
@@ -1,51 +1,45 @@
-import os
+import os, pdb
 
 
 base_path = os.getcwd()
-files = os.popen('find . -maxdepth 1 -type f')
 
-for file in files:
+# pdb.set_trace()
 
-	file = file.replace('./', '').replace('\n', '')
-	_,file_ext = os.path.splitext(file)
-	
-	if file_ext in ['.txt']:
+for dirpath, dirnames, filenames in os.walk(base_path):
 
-		if not os.path.exists('./text_files'):
-			os.makedirs('./text_files')
-			os.rename(os.path.join(base_path, file), os.path.join(base_path, "text_files", file))
-		os.rename(os.path.join(base_path, file), os.path.join(base_path, "text_files", file))
+	for file in filenames:
 
-	elif file_ext in ['.jpg', 'png']:
-		
-		if not os.path.exists('./photos'):
-			os.makedirs('./photos')
-			os.rename(os.path.join(base_path, file), os.path.join(base_path, "photos", file))
-		os.rename(os.path.join(base_path, file), os.path.join(base_path, "photos", file))
+		file_name, file_ext = os.path.splitext(file) # splits file name and file extension
 
-	elif file_ext in ['.docx', '.pdf', '.xlsx', '.xls', '.ppt']:
+		if file_ext.lower() in ['.txt']: # lower() makes sure .txt and .TXT are goes inside this if block
+			if not os.path.exists('./text_files'):				
+				os.makedirs('./text_files')
+				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "text_files", file))
+			os.rename(os.path.join(dirpath, file), os.path.join(base_path, "text_files", file))
 
-		if not os.path.exists('./documents'):
-			os.makedirs('./documents')
-			os.rename(os.path.join(base_path, file), os.path.join(base_path, "documents", file))
-		os.rename(os.path.join(base_path, file), os.path.join(base_path, "documents", file))
+		elif file_ext.lower() in ['.jpg', 'png']:
+			if not os.path.exists('./images'):				
+				os.makedirs('./images')
+				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "images", file))
+			os.rename(os.path.join(dirpath, file), os.path.join(base_path, "images", file))
 
-	elif file_ext in ['.deb', '.exe']:
+		elif file_ext.lower() in ['.docx', '.pdf', '.xlsx', '.xls', '.ppt']:
+			if not os.path.exists('./documents'):				
+				os.makedirs('./documents')
+				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "documents", file))
+			os.rename(os.path.join(dirpath, file), os.path.join(base_path, "documents", file))
 
-		if not os.path.exists('./packages'):
-			os.makedirs('./packages')
-			os.rename(os.path.join(base_path, file), os.path.join(base_path, "packages", file))
-		os.rename(os.path.join(base_path, file), os.path.join(base_path, "packages", file))
+		elif file_ext.lower() in ['.deb', '.exe']:
+			if not os.path.exists('./packages'):				
+				os.makedirs('./packages')
+				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "packages", file))
+			os.rename(os.path.join(dirpath, file), os.path.join(base_path, "packages", file))
 
-	elif file_ext in ['.py', '.pyc']:
+		elif file_ext.lower() in ['.py', '.pyc']:
+			pass
 
-		pass
-
-	else:
-
-		if not os.path.exists('./others'):
-			os.makedirs('./others')
-			os.rename(os.path.join(base_path, file), os.path.join(base_path, "others", file))
-		os.rename(os.path.join(base_path, file), os.path.join(base_path, "others", file))
-
-print('Execution Ends !')
+		else:
+			if not os.path.exists('./others'):				
+				os.makedirs('./others')
+				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "others", file))
+			os.rename(os.path.join(dirpath, file), os.path.join(base_path, "others", file))

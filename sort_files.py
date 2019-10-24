17767fbf9b2895aa836ae279ab528809cacf5279
hashi <kumarnisit@gmail.com>
2019-10-24 12:51:32 +0530

    added FileExistsError exception

--- sort_files.py
+++ sort_files.py
@@ -3,7 +3,8 @@ import os
 
 base_path = os.getcwd()
 
-txt_counter,img_counter,doc_counter,package_counter,video_counter,other_counter=0, 0, 0, 0, 0, 0
+txt_counter,img_counter,doc_counter,package_counter,video_counter,other_counter = 0, 0, 0, 0, 0, 0
+txt_dupli, img_dupli, doc_dupli, pack_dupli, vid_dupli, other_dupli = 0, 0, 0, 0, 0, 0
 
 for dirpath, dirnames, filenames in os.walk(base_path):
 
@@ -12,44 +13,95 @@ for dirpath, dirnames, filenames in os.walk(base_path):
 		file_name, file_ext = os.path.splitext(file) # splits file name and file extension
 
 		if file_ext.lower() in ['.txt']: # lower() makes sure .txt and .TXT are goes inside this if block
-			txt_counter += 1
 			if not os.path.exists('./text_files'):				
 				os.makedirs('./text_files')
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "text_files", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "text_files", file))
+				except FileExistsError:
+					txt_dupli += 1
+					# renaming and moving file works only on windows, on UNIX systems it replaces same files silently
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "text_files", file_name+'('+'copy'+str(txt_dupli)+')'+file_ext))
 			else:
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "text_files", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "text_files", file))
+				except FileExistsError:
+					txt_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "text_files", file_name+'('+'copy'+str(txt_dupli)+')'+file_ext))
 
 		elif file_ext.lower() in ['.jpg', '.png']:
 			img_counter += 1
 			if not os.path.exists('./images'):				
 				os.makedirs('./images')
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "images", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "images", file))
+				except FileExistsError:
+					img_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "images", file_name+'('+'copy'+str(img_dupli)+')'+file_ext))
+
 			else:
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "images", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "images", file))
+				except FileExistsError:
+					img_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "images", file_name+'('+'copy'+str(img_dupli)+')'+file_ext))
 
 		elif file_ext.lower() in ['.docx', '.pdf', '.xlsx', '.xls', '.ppt']:
 			doc_counter += 1
 			if not os.path.exists('./documents'):				
 				os.makedirs('./documents')
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "documents", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "documents", file))
+				except FileExistsError:
+					doc_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "documents", file_name+'('+'copy'+str(doc_dupli)+')'+file_ext))
 			else:
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "documents", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "documents", file))
+				except FileExistsError:
+					doc_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "documents", file_name+'('+'copy'+str(doc_dupli)+')'+file_ext))
 
 		elif file_ext.lower() in ['.deb', '.exe']:
 			package_counter += 1
 			if not os.path.exists('./packages'):				
 				os.makedirs('./packages')
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "packages", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "packages", file))
+				except FileExistsError:
+					pack_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "packages", file_name+'('+'copy'+str(pack_dupli)+')'+file_ext))
 			else:
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "packages", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "packages", file))
+				except FileExistsError:
+					pack_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "packages", file_name+'('+'copy'+str(pack_dupli)+')'+file_ext))
 
 		elif file_ext.lower() in ['.mp4', '.mkv']:
 			video_counter += 1
-			if not os.path.exists('./videos'):				
+			if not os.path.exists('./videos'):
 				os.makedirs('./videos')
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "videos", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "videos", file))
+				except FileExistsError:
+					vid_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "videos", file_name+'('+'copy'+str(vid_dupli)+')'+file_ext))
 			else:
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "videos", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "videos", file))
+				except FileExistsError:
+					vid_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "videos", file_name+'('+'copy'+str(vid_dupli)+')'+file_ext))
 
 		elif file_ext.lower() in ['.py', '.pyc']:
 			pass
@@ -58,9 +110,20 @@ for dirpath, dirnames, filenames in os.walk(base_path):
 			other_counter += 1
 			if not os.path.exists('./others'):		
 				os.makedirs('./others')
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "others", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "others", file))
+				except FileExistsError:
+					other_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "others", file_name+'('+'copy'+str(other_dupli)+')'+file_ext))
+				
 			else:
-				os.rename(os.path.join(dirpath, file), os.path.join(base_path, "others", file))
+				try:
+					os.rename(os.path.join(dirpath, file), os.path.join(base_path, "others", file))
+				except FileExistsError:
+					other_dupli += 1
+					os.rename(os.path.join(dirpath, file),\
+					 os.path.join(base_path, "others", file_name+'('+'copy'+str(other_dupli)+')'+file_ext))
 
 print(f'Total files moved: {txt_counter+img_counter+doc_counter+package_counter+video_counter+other_counter}\
  	\nTextFiles Moved:{txt_counter}\nImageFiles Moved:{img_counter}\
